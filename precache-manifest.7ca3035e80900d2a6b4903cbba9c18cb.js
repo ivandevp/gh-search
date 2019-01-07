8ed0da2203e7427b2e44d2afe3ebdd7df45810c8
Ivan Medina <medinadiazivan@gmail.com>
2019-01-06 23:47:28 -0600

    Updates

---
+++ precache-manifest.7ca3035e80900d2a6b4903cbba9c18cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3b90ce547f4f7f62736",
+    "url": "/gh-search/static/js/runtime~main.b3b90ce5.js"
+  },
+  {
+    "revision": "a2c97a99818ecb39d55d",
+    "url": "/gh-search/static/js/main.a2c97a99.chunk.js"
+  },
+  {
+    "revision": "b82a0fa060477566a085",
+    "url": "/gh-search/static/js/1.b82a0fa0.chunk.js"
+  },
+  {
+    "revision": "a2c97a99818ecb39d55d",
+    "url": "/gh-search/static/css/main.d7e02ff9.chunk.css"
+  },
+  {
+    "revision": "043baa8b92ea947e7f896f35a79ada21",
+    "url": "/gh-search/index.html"
+  }
+];
